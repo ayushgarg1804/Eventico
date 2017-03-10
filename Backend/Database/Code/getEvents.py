c35bb5f2383f28143dad4f8323a95631a43578bc
Ayush Garg <ayushgarg1804@gmail.com>
2017-03-10 13:56:25 +0530

    pagination handled and json downloaded

--- Backend/Database/Code/getEvents.py
+++ Backend/Database/Code/getEvents.py
@@ -1,5 +1,6 @@
 import requests as rq
 import json
+import os
 
 token = "token=JUEUY62BK5ZOWANSKAJX"
 
@@ -8,4 +9,20 @@ url = "https://www.eventbriteapi.com/v3/events/search/?"
 expansions = "expand=logo,venue,organizer,format,category,subcategory,bookmark_info"
 response = rq.get(url + parameters + '&' + expansions + '&' + token)
 
-#Requires saving paginated responses
+#Requires saving paginated responses : done
+
+res = response.json()
+num_pages = res['pagination']['page_count']
+print num_pages
+dirpath = '../Json/'
+if not os.path.exists(dirpath):
+	os.makedirs(dirpath)
+
+with open(dirpath + 'Events_page_1.json', 'w') as file:
+	json.dump(res, file)
+
+for page in range(2, num_pages + 1):
+    res = rq.get(url + parameters + '&' + expansions + '&' + token, params={'page': page}).json()
+    print page
+    with open(dirpath + 'Events_page_' + str(page) + '.json', 'w') as file:
+		json.dump(res, file)
