7598694a468a6b122e23abf2f813e74012c7d822
Aman Priyadarshi <aman.eureka@gmail.com>
2017-03-21 01:02:31 +0530

    added login-register.html

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 # @Author: Aman Priyadarshi
 # @Date:   2017-03-20 20:09:41
 # @Last Modified by:   Aman Priyadarshi
-# @Last Modified time: 2017-03-20 20:32:50
+# @Last Modified time: 2017-03-21 01:01:46
 
 from flask import Flask, render_template
 
@@ -13,6 +13,11 @@ from util import assets
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/login')
+@app.route('/register')
+def login():
+	return render_template('login-register.html')
 
 @app.errorhandler(404)
 def not_found(e):
--- src/main.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Aman Priyadarshi
-# @Date:   2017-03-20 19:37:00
-# @Last Modified by:   Aman Priyadarshi
-# @Last Modified time: 2017-03-20 20:28:45
-
