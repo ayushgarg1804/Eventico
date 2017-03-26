ab6e622f583bef9fefd6db8ae28adafca6182777
Aman Priyadarshi <aman.eureka@gmail.com>
2017-03-26 16:15:53 +0530

    herku configuration - added app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: Aman Priyadarshi
+# @Date:   2017-03-26 16:13:42
+# @Last Modified by:   Aman Priyadarshi
+# @Last Modified time: 2017-03-26 16:14:19
+
+import os
+
+from src.util import database
+database.sql_init()
+
+from src import app
+app.secret_key = os.urandom(12)
+app.run(port=80)
