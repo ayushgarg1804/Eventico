d2ba0e42adaba3172ee73a6b33be031116ec1fb9
Aman Priyadarshi <aman.eureka@gmail.com>
2017-03-26 17:12:23 +0530

    deployment debug = True

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # @Author: Aman Priyadarshi
 # @Date:   2017-03-26 16:13:42
 # @Last Modified by:   Aman Priyadarshi
-# @Last Modified time: 2017-03-26 16:14:19
+# @Last Modified time: 2017-03-26 17:12:09
 
 import os
 
@@ -11,4 +11,4 @@ database.sql_init()
 
 from src import app
 app.secret_key = os.urandom(12)
-app.run(port=80)
+app.run(port=80,debug=True)
