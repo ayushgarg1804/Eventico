282524e5c5db957cc6c928daf50c05183d7016b5
Ayush Garg <ayushgarg1804@gmail.com>
2017-03-27 14:59:45 +0530

    port change

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # @Author: Aman Priyadarshi
 # @Date:   2017-03-26 16:13:42
 # @Last Modified by:   Ayush Garg
-# @Last Modified time: 2017-03-27 11:26:06
+# @Last Modified time: 2017-03-27 14:59:24
 
 import os
 
@@ -11,4 +11,4 @@ database.sql_init()
 
 from src import app
 app.secret_key = os.urandom(12)
-app.run(debug=True)
+app.run(port = 80, debug=True)
