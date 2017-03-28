639a3b37797af0bfd3af07e3ea5f8cd52ba345f2
Aman Priyadarshi <aman.eureka@outlook.com>
2017-03-28 21:24:31 +0530

    removed flask app -- heroku testing

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 # @Author: Aman Priyadarshi
 # @Date:   2017-03-26 16:13:42
 # @Last Modified by:   amaneureka
-# @Last Modified time: 2017-03-28 20:54:28
+# @Last Modified time: 2017-03-28 22:27:49
 
 import os
 
 from src.util import database
-database.sql_init()
-
 from src import app
+
 app.secret_key = os.urandom(12)
-app.run(debug=True)
+
+if __name__ == '__main__':
+	database.sql_init()
+	app.run(debug=True)
